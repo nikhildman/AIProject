edf8bb20f8f32d483d09b094d513af8a75c69c72
nikhildman <91721536+nikhildman@users.noreply.github.com>
2023-04-20 15:22:03 +0530

    Update demo_release.py

--- demo_release.py
+++ demo_release.py
@@ -1,4 +1,4 @@
-
+# Importing necessary modules and defining command line arguments
 import argparse
 import matplotlib.pyplot as plt
 
@@ -10,28 +10,29 @@ parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
 parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
 opt = parser.parse_args()
 
-# load colorizers
+# Loading two pre-trained colorizers - ECCV 2016 and SIGGRAPH 2017
 colorizer_eccv16 = eccv16(pretrained=True).eval()
 colorizer_siggraph17 = siggraph17(pretrained=True).eval()
+# Setting up the colorizers to use GPU if specified in the command line argument
 if(opt.use_gpu):
 	colorizer_eccv16.cuda()
 	colorizer_siggraph17.cuda()
-
-# default size to process images is 256x256
-# grab L channel in both original ("orig") and resized ("rs") resolutions
+# Preprocessing the input image to obtain its L channel in original and resized resolutions
 img = load_img(opt.img_path)
 (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+# Setting up the resized L channel to use GPU if specified in the command line argument
 if(opt.use_gpu):
 	tens_l_rs = tens_l_rs.cuda()
 
-# colorizer outputs 256x256 ab map
-# resize and concatenate to original L channel
+# Feeding the L channel to both colorizers to obtain their respective AB channels
 img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
 out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
 out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+# Concatenating the resized AB channels with the original L channel to obtain the final colorized image
 
 plt.imsave('%s_eccv16.png'%opt.save_prefix, out_img_eccv16)
 plt.imsave('%s_siggraph17.png'%opt.save_prefix, out_img_siggraph17)
+# Saving the output images with the specified prefix and showing the original, input, and output images for comparison
 
 plt.figure(figsize=(12,8))
 plt.subplot(2,2,1)
